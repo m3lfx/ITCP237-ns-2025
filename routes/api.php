d7ece834e5552cb3d87f71382b9e001320f97f70
rommel dalisay <rommelmdalisay@gmail.com>
2025-05-24 10:27:55 +0800

    pie chart

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::apiResource('customers', CustomerController::class);
 Route::apiResource('items', ItemController::class);
 Route::get('/dashboard/address-chart',[DashboardController::class, 'addressChart']);
 Route::get('/dashboard/sales-chart',[DashboardController::class, 'salesChart' ]);
+Route::get('/dashboard/items-chart',[DashboardController::class, 'itemsChart']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
